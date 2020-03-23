3adc98715ebf2a343d40d2ca64e2df9c5ccd49a3
Волков Дмитрий <div@gcfs.lan>
2020-03-23 16:10:54 +0400

    Переосмысление концепции.

--- SrvReplacement.ps1
+++ SrvReplacement.ps1
@@ -1,37 +1,37 @@
-#Current version 0.0.1
+#Current version 0.0.4
 
 Write-Host "Выберите действие"
 Write-Host "1 - Копирование данных сервера"
-Write-Host "2 - Востановление данных сервера"
+Write-Host "2 - Востановление данных сервера влдатутаушц"
 
 $Action = read-host
 
-#Глобальные переменные
-$DesPath = "\\w-it3\backup\"
-
 #Функция проверки доступности пути для копирования или востоновления
-function IsPath {
-    $IsPath = Test-Path $DesPath$KassNum
-    if ($IsPath -eq "True") 
-    { Write-Host "Путь найден" }
-    else {
-        Write-Host "Путь не найден"
-    }
-}
 
 #Если выбрано компирование.
 if ($Action -eq 1) {
-    Write-Host "Введите номер кассы на которую нужно скопировать данные"
-    $KassNum = Read-Host
-    IsPath
-    
+    Write-Host "Введите сетевой путь коталога в который необходимо скопировать данные сервера"
+    $Destination = Read-Host
+    $IsPath = Test-Path $Destination
+    if ($IsPath -eq "True") {
+        Write-Host "все хоршо"
+    }
+    else {
+        Write-Host "Все плохо"
+    }
 }
 
 #Если выбрано востановление.
 elseif ($Action -eq 2) {
-    Write-Host "Введите номер кассы с которой нужно востановить данные "
-    $KassNum = Read-Host
-    IsPath
+    Write-Host "Введите сетевой путь коталога из которого необходимо востановить данные весвера"
+    $Destination = Read-Host
+    $IsPath = Test-Path $Destination
+    if ($IsPath -eq "True") {
+        Write-Host "все хоршо"
+    }
+    else {
+        Write-Host "Все плохо"
+    }
 }
 
 #Если выбран вариант отлиный от предложанных.
